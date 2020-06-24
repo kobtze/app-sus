a153ad2c348c26515011abbf18fbd41273f143d7
bar varfman <barrestyle@gmail.com>
2020-06-24 20:46:37 +0300

    bar update day 1

---
+++ js/apps/email/pages/pages/email-app.js
@@ -0,0 +1,13 @@
+export default {
+  template: `
+        <section class="email-app">
+            <h2>Email App</h2>
+           <nav>
+        <!-- <router-link to=""/> -->
+           </nav>
+        </section>
+        `,
+  created() {
+    console.log("email-app loaded");
+  },
+};
---
+++ js/apps/email/pages/sevices/email-service.js
@@ -0,0 +1,13 @@
+gEmails = [
+  {
+    subject: "Wassap?",
+    body: "Pick up!",
+    isRead: false,
+    sentAt: 1551133930594,
+  },
+];
+
+
+
+
+
